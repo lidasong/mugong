53e3daf44967ebcd2abc60e59c16879609aaf68b
李大松 <hzlidasong@corp.netease.com>
2018-03-21 20:18:11 +0800

    deploy hexo

--- js/share.js
+++ js/share.js
@@ -1,9 +1,9 @@
-(function($){
+(function($) {
 
   // article-share
-  $('body').on('click', function(){
+  $('body').on('click', function() {
     $('.article-share-box.on').removeClass('on');
-  }).on('click', '.article-share-link', function(e){
+  }).on('click', '.article-share-link', function(e) {
     e.stopPropagation();
 
     var $this = $(this),
@@ -14,24 +14,24 @@
       title = document.title,
       offset = $this.offset();
 
-    if ($('#' + id).length){
+    if ($('#' + id).length) {
       var box = $('#' + id);
 
-      if (box.hasClass('on')){
+      if (box.hasClass('on')) {
         box.removeClass('on');
         return;
       }
     } else {
       var html = [
         '<div id="' + id + '" class="article-share-box">',
-          '<input class="article-share-input" value="' + url + '">',
-          '<div class="article-share-links">',
-            '<a href="//twitter.com/intent/tweet?url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
-            '<a href="//www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
-            '<a href="//service.weibo.com/share/share.php?title=' + title + '&url=' + encodedUrl + '&searchPic=true&style=number' + '" class="article-share-weibo" target="_blank" title="Weibo"></a>',
-            '<a href="' + qrcode_img + '" class="article-share-qrcode" target="_blank" title="QR code"></a>',
-            '<div class="qrcode"><img src=' + qrcode_img + '></div>',
-          '</div>',
+        '<input class="article-share-input" value="' + url + '">',
+        '<div class="article-share-links">',
+        '<a href="//twitter.com/intent/tweet?url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
+        '<a href="//www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
+        '<a href="//service.weibo.com/share/share.php?title=' + title + '&url=' + encodedUrl + '&searchPic=true&style=number' + '" class="article-share-weibo" target="_blank" title="Weibo"></a>',
+        '<a href="' + qrcode_img + '" class="article-share-qrcode" target="_blank" title="QR code"></a>',
+        '<div class="qrcode"><img src=' + qrcode_img + '></div>',
+        '</div>',
         '</div>'
       ].join('');
 
@@ -46,11 +46,11 @@
       top: offset.top + 25,
       left: offset.left
     }).addClass('on');
-  }).on('click', '.article-share-box', function(e){
+  }).on('click', '.article-share-box', function(e) {
     e.stopPropagation();
-  }).on('click', '.article-share-box-input', function(){
+  }).on('click', '.article-share-box-input', function() {
     $(this).select();
-  }).on('click', '.article-share-box-link', function(e){
+  }).on('click', '.article-share-box-link', function(e) {
     e.preventDefault();
     e.stopPropagation();
 
